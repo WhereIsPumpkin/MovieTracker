241356a9c9b3fc24c2d5c8091c64d0e289e3641c
AnriBeridzee <aberidze02@gmail.com>
2024-01-07 19:36:30 +0400

    Add TV series view model code

--- MovieTracker App/Scenes/TV Series/TvSeriesVM.swift
+++ MovieTracker App/Scenes/TV Series/TvSeriesVM.swift
@@ -10,4 +10,33 @@ import NetSwift
 
 class TvSeriesVM: ObservableObject {
     
+    // MARK: - Properties
+    @Published var tvSeries: [TvSeries] = []
+    @Published var errorMessage: String = ""
+    private let baseURL = "https://api.themoviedb.org/3/tv/popular"
+    private let apiKey = "0ef1f9d4f31d9ebccb49105074d05775"
+    
+    
+    // MARK: - Init
+    init() {
+        fetchData()
+    }
+    
+    
+    // MARK: - Methods
+    private func fetchData() {
+        guard let url = URL(string: "\(baseURL)?api_key=\(apiKey)") else {
+            errorMessage = "Invalid URL"
+            return
+        }
+        
+        NetworkManager.shared.fetchDecodableData(from: url, responseType: TvSeriesResponse.self) { [self] result in
+            switch result {
+            case .success(let data):
+                self.tvSeries = data.results
+            case .failure(let error):
+                self.errorMessage = error.localizedDescription
+            }
+        }
+    }
 }
